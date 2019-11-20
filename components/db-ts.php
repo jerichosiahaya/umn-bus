586a4108ae38e0193f59b897ed114d45213064f3
Je <jericho.siahaya@gmail.com>
2019-11-20 22:40:15 +0700

    Delete db-ts.php

--- components/db-ts.php
+++
@@ -1,8 +0,0 @@
-<?php
-// konfigurasi database tanpa session
-$host       =   "localhost";
-$user       =   "root";
-$password   =   "";
-$database   =   "u-bus";
-// perintah php untuk akses ke database
-$koneksi = mysqli_connect($host, $user, $password, $database);
